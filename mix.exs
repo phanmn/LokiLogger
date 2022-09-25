1045a9fdf84e763f46c59c6c65bd63baf9db43a9
Nhut Phan <thanquan@gmail.com>
2022-09-25 16:59:31 +0700

    feat: jason -> 1.4

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule LokiLogger.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.3.0"},
+      {:jason, "~> 1.4.0"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:benchee, "~> 1.1.0", only: :test},
       {:tesla, "~> 1.4.0"},
